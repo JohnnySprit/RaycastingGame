5fae8ab14405dcaa5c600c742dbfc4892e1c7fa2
loafyreal <166061723+loafyreal@users.noreply.github.com>
2024-04-21 21:43:08 -0400

    Update Camera.java

--- RaycastingGame/Raycaster/src/Camera.java
+++ RaycastingGame/Raycaster/src/Camera.java
@@ -6,13 +6,53 @@ public class Camera {
     private double x, y;
     private List<Ray> RayObjects;
     private double angle;
+    private int numRays;
+    private int fov;
+    
     public Camera(double x, double y, List<Ray> RayObjects, double angle) {
         this.x = x;
         this.y = y;
         this.RayObjects = RayObjects;
         this.angle = 0;
         this.state = CameraState.STANDSTILL;
-    }
+        this.numRays = numRays;
+        this.fov = fov;
 
-    public void update()
-}
+        CameraMotionListener motionListener = new CameraMotionListener();
+    }
+    private class CameraMotionListener extends MouseAdapter {
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            x = e.getX();
+            y = e.getY();
+        }
+        public CameraMotionListener getCameraMotionListener() {
+            return new CameraMotionListener();
+        }
+
+        public void projectRays() {
+            RayObjects.clear();
+            double angleStep = (double) fov / numRays;
+            double startAngle = angle - (fov / 2.0);
+
+            for (int i = 0; i < numRays; i++) {
+                double rayAngle = startAngle + i * angleStep;
+                Ray ray = new Ray(x, y, rayAngle);
+                RayObjects.add(ray);
+            }
+            public void updateAngle(double deltaX, double deltaY) {
+                double dx = deltaX * 0.005;
+                double dy = deltaY * 0.005;
+
+                angle += dx;
+            }
+
+            public List<Ray> getRays() {
+                return RayObjects;
+            }
+
+            public double getAngle() {
+                return angle;
+            }
+        }
+    }
