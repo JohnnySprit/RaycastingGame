acb5dc6831a8fb621a4aac866bacbff5c4d94592
loafyreal <166061723+loafyreal@users.noreply.github.com>
2024-04-22 16:53:38 -0400

    Update CameraState.java

--- CameraState.java
+++ CameraState.java
@@ -1,5 +1,6 @@
 public final class CameraState {
-
+    
+    // Constants representing different states of camera movement
     public static final int STANDSTILL = 0;
     public static final int FORWARD_WALK = 1;
     public static final int BACKWARD_WALK = 2;
@@ -7,6 +8,13 @@ public final class CameraState {
     public static final int LEFT_TURN = 4;
     public static final int RIGHT_TURN = 8;
 
+    /**
+     * Check if a specific flag is enabled in the given mask.
+     *
+     * @param flag The flag to check
+     * @param mask The mask containing the flag
+     * @return true if the flag is enabled in the mask, false otherwise
+     */
     private CameraState() {
     }
     public static boolean isFlagEnabled(final int flag, final int mask) {
