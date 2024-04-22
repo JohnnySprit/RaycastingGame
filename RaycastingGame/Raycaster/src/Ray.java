18c01c6cce2be880235a22b5f11fb5c9370a90f5
loafyreal <166061723+loafyreal@users.noreply.github.com>
2024-04-22 12:45:04 -0400

    Update Ray.java

--- RaycastingGame/Raycaster/src/Ray.java
+++ RaycastingGame/Raycaster/src/Ray.java
@@ -1,11 +1,29 @@
 import java.awt.*;
 import java.awt.geom.Line2D;
 
-public class Ray {
-    private Line2D.Double line;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 
-    public void drawRay(final Graphics2D g2) {
-        g2.setColor(Color.WHITE);
-        g2.draw(line);
+public class Ray extends Line2D.Double {
+    private double angle;
+    private double distance;
+
+    public Ray(double x1, double y1, double x2, double y2, double angle, double distance) {
+        super(x1, y1, x2, y2);
+        this.angle = angle;
+        this.distance = distance;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void drawRay(Graphics2D g2) {
+        g2.setColor(java.awt.Color.WHITE);
+        g2.draw(this);
     }
 }
