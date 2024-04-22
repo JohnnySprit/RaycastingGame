d6937e41b19fe1ee419507ac20d86882fb0c4d4a
loafyreal <166061723+loafyreal@users.noreply.github.com>
2024-04-22 12:58:58 -0400

    Update RaycasterProjectionPanel.java

--- RaycastingGame/Raycaster/src/RaycasterProjectionPanel.java
+++ RaycastingGame/Raycaster/src/RaycasterProjectionPanel.java
@@ -1,19 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 public class RaycasterProjectionPanel extends JPanel {
 
-    /**
-     * Root driver object to keep track of sizing.
-     */
     private final RaycasterRunner RUNNER;
-
-    /**
-     * Overhead panel to access the generated rays.
-     */
     private final RaycasterPanel RAYCASTER_PANEL;
-    private final Color myColor = new Color(111, 67, 188);
-
+    private final Color WALL_COLOR = Color.WHITE;
 
     public RaycasterProjectionPanel(final RaycasterRunner raycasterRunner, final RaycasterPanel raycasterPanel) {
         this.RUNNER = raycasterRunner;
@@ -21,14 +14,26 @@ public class RaycasterProjectionPanel extends JPanel {
         this.RAYCASTER_PANEL = raycasterPanel;
     }
 
-    public void update() {
+    public void project(Graphics2D g2d) {
+        Camera camera = this.RAYCASTER_PANEL.getCamera();
+        if (camera != null) {
+            List<Ray> rays = camera.getRays();
+            if (rays != null) {
+                for (Ray ray : rays) {
+                    g2d.setColor(Color.LIGHT_GRAY);
+                    g2d.draw(ray);
+                }
+            }
+        }
     }
 
     @Override
     public void paintComponent(final Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(myColor);
+        g2d.setColor(Color.BLACK);
         g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+        project(g2d);
     }
 }
