57586fcfc185214e2b028fdcee2804e428a6b20a
loafyreal <166061723+loafyreal@users.noreply.github.com>
2024-04-22 13:01:00 -0400

    Update RectangleObject.java

--- RaycastingGame/Raycaster/src/RectangleObject.java
+++ RaycastingGame/Raycaster/src/RectangleObject.java
@@ -21,4 +21,28 @@ public class RectangleObject implements Drawable {
         g2d.setColor(color);
         g2d.drawRect((int) x, (int) y, (int) width, (int) height);
     }
+    public Point2D.Double rectIntersection(final Ray ray) {
+        Line2D.Double[] sides = {
+                new Line2D.Double(x, y, x + width, y), // Top side
+                new Line2D.Double(x + width, y, x + width, y + height), // Right side
+                new Line2D.Double(x + width, y + height, x, y + height), // Bottom side
+                new Line2D.Double(x, y + height, x, y) // Left side
+        };
+
+        Point2D.Double closestIntersection = null;
+        double closestDistance = Double.MAX_VALUE;
+
+        for (Line2D.Double side : sides) {
+            Point2D.Double intersection = RaycasterUtils.intersection(ray, side);
+            if (intersection != null) {
+                double distance = intersection.distance(ray.x1, ray.y1);
+                if (distance < closestDistance) {
+                    closestDistance = distance;
+                    closestIntersection = intersection;
+                }
+            }
+        }
+
+        return closestIntersection;
+    }
 }
