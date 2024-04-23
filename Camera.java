f4cfe8a40bf3e86080fa0b85da32a00090ae5e28
loafyreal <166061723+loafyreal@users.noreply.github.com>
2024-04-23 14:34:59 -0400

    Update Camera.java

--- Camera.java
+++ Camera.java
@@ -84,22 +84,21 @@ public class Camera {
             }
             else if (keyCode == KeyEvent.VK_A) {
                 // Move camera left
-                x -= moveStep * Math.cos(Math.toRadians(angle));
-                y -= moveStep * Math.sin(Math.toRadians(angle));
+                x -= moveStep;
             } else if (keyCode == KeyEvent.VK_D) {
                 // Move camera right
-                x += moveStep * Math.cos(Math.toRadians(angle));
-                y += moveStep * Math.sin(Math.toRadians(angle));
+                x += moveStep;
+                
             }
             else if (keyCode == KeyEvent.VK_W) {
             // Move camera up
 
-                y -= moveStep * Math.cos(Math.toRadians(angle));
+                y -= moveStep;
 
         }
             else if (keyCode == KeyEvent.VK_S){
                 //move camera down
-                y+=moveStep*Math.cos(Math.toRadians(angle));
+                y+=moveStep;
             }
         }
     }
@@ -170,7 +169,7 @@ public class Camera {
     // Method to draw the camera as a yellow circle and its emitted rays
     public void drawCamera(Graphics2D g2d) {
         g2d.setColor(Color.YELLOW);
-        g2d.fill(new Ellipse2D.Double(x, y, 10, 10));
+        g2d.fill(new Ellipse2D.Double(x-5, y-5, 10, 10));
         // Draw rays
         drawRays(g2d);
     }
