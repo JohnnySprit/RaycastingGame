4268a28bdf6ed1cba945d2e19c8517edf2a2ac26
JohnnySprit <165676823+JohnnySprit@users.noreply.github.com>
2024-04-23 16:46:38 -0400

    Add files via upload

--- RaycasterProjectionPanel.java
+++ RaycasterProjectionPanel.java
@@ -1,18 +1,32 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 public class RaycasterProjectionPanel extends JPanel {
 
     private final RaycasterRunner RUNNER;
     private final RaycasterPanel RAYCASTER_PANEL;
-    private final Color WALL_COLOR = Color.WHITE;
+    private BufferedImage wallTexture; // Texture for the walls
+
 
     public RaycasterProjectionPanel(final RaycasterRunner raycasterRunner, final RaycasterPanel raycasterPanel) {
         this.RUNNER = raycasterRunner;
         this.setPreferredSize(new Dimension(this.RUNNER.getWidth() / 2, this.RUNNER.getHeight()));
         this.RAYCASTER_PANEL = raycasterPanel;
+        try {
+            // Replace "C:/path/to/your/image.jpg" with the actual absolute path to your image file
+            String absolutePath = "C:\\Users\\Johnny\\IdeaProjects\\RaycastingGame-main\\wall-texture.jpg";
+            wallTexture = ImageIO.read(new File(absolutePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void project(Graphics2D g2d) {
@@ -41,9 +55,14 @@ public class RaycasterProjectionPanel extends JPanel {
                     double leftX = rays.indexOf(ray) * wallWidth;
                     double rightX = leftX + wallWidth;
 
-                    // Draw the wall segment
-                    g2d.setColor(Color.GRAY);
-                    g2d.draw(new Line2D.Double(leftX, wallY, rightX, wallY + wallHeight));
+                    // Create a texture paint with the wall texture
+                    TexturePaint texturePaint = new TexturePaint(wallTexture, new Rectangle(0, 0, wallTexture.getWidth(), wallTexture.getHeight()));
+
+                    // Set the texture paint for filling the wall segment
+                    g2d.setPaint(texturePaint);
+
+                    // Fill the wall segment with the texture
+                    g2d.fill(new Rectangle2D.Double(leftX, wallY, wallWidth, wallHeight));
                 }
             }
         }
@@ -53,7 +72,7 @@ public class RaycasterProjectionPanel extends JPanel {
     public void paintComponent(final Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.BLACK);
+        g2d.setColor(Color.black);
         g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
 
         project(g2d);
